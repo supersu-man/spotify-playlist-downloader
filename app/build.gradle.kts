3094bf547c276d8c933f0660c5873f1881a13b56
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-11-07 00:26:26 +0530

    v1.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.sumanth.spd"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.7.1"
+        versionCode = 16
+        versionName = "1.7.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
