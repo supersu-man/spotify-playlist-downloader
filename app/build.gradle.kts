1835216dba4668f678856a32415b6488b2a7b566
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2025-01-05 01:15:00 +0530

    deps: updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "dev.sumanth.spd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.sumanth.spd"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 16
         versionName = "1.7.2"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -26,17 +23,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
     }
     packaging {
         resources {
@@ -46,19 +40,22 @@ android {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp-urlconnection:5.0.0-alpha.3")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.3.1")
+
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:5.0.0-alpha.14")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
     implementation("com.github.supersu-man:apkupdater-library:v2.0.1")
     implementation("se.michaelthelin.spotify:spotify-web-api-java:8.0.0")
-    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.2")
+    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
 }
