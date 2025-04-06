0cae6613d11a256c07db68489d5ab017526a7389
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2025-04-07 02:21:39 +0530

    v1.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.sumanth.spd"
         minSdk = 24
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.7.2"
+        versionCode = 17
+        versionName = "1.7.3"
     }
 
     buildTypes {
