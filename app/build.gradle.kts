19bd2169a2f7cb4cf734a618b20e39906ada562b
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2026-09-24 06:49:34 +0530

    v1.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.sumanth.spd"
         minSdk = 24
         targetSdk = 36
-        versionCode = 31
-        versionName = "1.13.0"
+        versionCode = 32
+        versionName = "1.14.0"
     }
 
     buildTypes {
